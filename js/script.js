3fba5fb97575f6e9bb7acc6fc4edbbb5c8716b35
Maccoffee-classik <33307173+Maccoffee-classik@users.noreply.github.com>
2023-05-11 17:57:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -98,3 +98,295 @@ $(document).ready(function(){
 
 
 
+$(document).ready(function(){
+	$('.slider2').slick({
+		dots:true,
+		slidesToShow:1,
+		autoplay: true,
+		autoplaySpeed:2000,
+	
+		
+		responsive: [
+			{ 
+				
+				breakpoint: 1240,
+				settings: {
+					slidesToShow:1
+
+				}
+				},{
+				
+				
+				breakpoint: 992,
+				settings: {
+					slidesToShow:1
+
+
+				}
+				},{
+
+
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+
+
+				}
+			
+			}
+
+
+
+
+
+		]
+
+	});
+
+
+});
+
+
+
+$(document).ready(function(){
+	$('.slider3').slick({
+		dots:true,
+		slidesToShow:1,
+		autoplay: true,
+		autoplaySpeed:2000,
+	
+		
+		responsive: [
+			{ 
+				
+				breakpoint: 1240,
+				settings: {
+					slidesToShow:1
+
+				}
+				},{
+				
+				
+				breakpoint: 992,
+				settings: {
+					slidesToShow:1
+
+
+				}
+				},{
+
+
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+
+
+				}
+			
+			}
+
+
+
+
+
+		]
+
+	});
+
+
+});
+
+$(document).ready(function(){
+	$('.slider4').slick({
+		dots:true,
+		slidesToShow:1,
+		autoplay: true,
+		autoplaySpeed:2000,
+	
+		
+		responsive: [
+			{ 
+				
+				breakpoint: 1240,
+				settings: {
+					slidesToShow:1
+
+				}
+				},{
+				
+				
+				breakpoint: 992,
+				settings: {
+					slidesToShow:1
+
+
+				}
+				},{
+
+
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+
+
+				}
+			
+			}
+
+
+
+
+
+		]
+
+	});
+
+
+});
+
+
+$(document).ready(function(){
+	$('.slider5').slick({
+		dots:true,
+		slidesToShow:1,
+		autoplay: true,
+		autoplaySpeed:2000,
+	
+		
+		responsive: [
+			{ 
+				
+				breakpoint: 1240,
+				settings: {
+					slidesToShow:1
+
+				}
+				},{
+				
+				
+				breakpoint: 992,
+				settings: {
+					slidesToShow:1
+
+
+				}
+				},{
+
+
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+
+
+				}
+			
+			}
+
+
+
+
+
+		]
+
+	});
+
+
+});
+
+
+
+$(document).ready(function(){
+	$('.slider6').slick({
+		dots:true,
+		slidesToShow:1,
+		autoplay: true,
+		autoplaySpeed:2000,
+	
+		
+		responsive: [
+			{ 
+				
+				breakpoint: 1240,
+				settings: {
+					slidesToShow:1
+
+				}
+				},{
+				
+				
+				breakpoint: 992,
+				settings: {
+					slidesToShow:1
+
+
+				}
+				},{
+
+
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+
+
+				}
+			
+			}
+
+
+
+
+
+		]
+
+	});
+
+
+});
+
+$(document).ready(function(){
+	$('.slider7').slick({
+		dots:true,
+		slidesToShow:1,
+		autoplay: true,
+		autoplaySpeed:2000,
+	
+		
+		responsive: [
+			{ 
+				
+				breakpoint: 1240,
+				settings: {
+					slidesToShow:1
+
+				}
+				},{
+				
+				
+				breakpoint: 992,
+				settings: {
+					slidesToShow:1
+
+
+				}
+				},{
+
+
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+
+
+				}
+			
+			}
+
+
+
+
+
+		]
+
+	});
+
+
+});
